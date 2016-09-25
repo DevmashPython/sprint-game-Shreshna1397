5b7a5897caa6a0d4f286c50c52933b58754875a2
Shreshna <shreshna.brahma@gmail.com>
2016-09-26 01:12:01 +0530

    pytshre

---
+++ 1st.py
@@ -0,0 +1,48 @@
+import msvcrt
+import time
+
+fin=10
+co=0
+a=0
+
+print"PRESS ENTER TO START"
+raw_input()
+s_time=time.time()
+while(1):
+	key=msvcrt.getch()
+	if key=='\xe0':
+		co=co+1
+		print"D",
+		if co==fin:
+			break	
+
+fin=10
+co=0
+while(1):
+	key=msvcrt.getch()
+	if key=='\xe0':
+		co=co+1
+		print"\n\t\t  S",
+		if co==fin:
+			break
+
+fin=10
+co=0
+while(1):
+	key=msvcrt.getch()
+	if key=='\xe0':
+		co=co+1
+		print"D",
+		if co==fin:
+			break
+
+print"\n"
+
+time_elapsed=time.time()-s_time
+print"\nCONGRATS YOU JUST FINISHED THE GAME!"
+print"TIME ELAPSED IS %s SECS"%str(round(time_elapsed))
+
+
+
+
+
